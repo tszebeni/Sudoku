05d732546b99eab14f6e239bf2a8822132c88dc7
Tamas Szebeni <khaliy@gmail.com>
2012-03-19 10:56:29 +0100

    added getjson

--- js/sudoku.js
+++ js/sudoku.js
@@ -9,6 +9,14 @@ $(document).ready(function() {
         this.cells = new Array(81);
     };
 
+    SudokuModel.prototype = {
+        load: function (id) {
+            $.getJSON("/service/sudoku.json",function () {
+                alert("success!");
+            });
+        }
+    };
+
     SudokuView = function () {};
 
     SudokuPresenter = function (view,model) {};
